44437f00e69fdf718a363182dd7f29a452d13860
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2022-03-11 16:13:59 +0530

    basic example of useRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./styles.css";
+import { useRef, useState } from "react";
 
 export default function App() {
+  const inputValue = useRef(null);
+  const [inputText, setInputText] = useState("i am input");
+  console.log("inputValue", inputValue);
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    inputValue.current.focus();
+    alert("i am submitted");
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Lets learn about useRef hook</h1>
+      <form>
+        <input
+          type="text"
+          ref={inputValue}
+          placeholder="current prop"
+          value={inputText}
+        />
+        <button onClick={onSubmitHandler}>Focus Input</button>
+      </form>
     </div>
   );
 }
